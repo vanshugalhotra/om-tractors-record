9a1f7a13b50833c20352037d970a5a6a2365962b
Vanshu Galhotra <vanshugalhotra3332@gmail.com>
2024-07-16 11:25:31 +0530

    feat - added utility funcs

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,11 @@
 import { Inter } from "next/font/google";
 import { SidebarProvider } from "@/context/SidebarContext";
+import { LoadingProvider } from "@/context/LoadingContext";
 import Layout from "@/components/Layout/Layout";
 import "./globals.css";
+import NextTopLoader from "nextjs-toploader";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,12 +18,37 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <SidebarProvider>
-          <Layout>
-            {children}
-            <section className="my-[20vh]"></section>
-          </Layout>
-        </SidebarProvider>
+        <NextTopLoader
+          color="#FF9F43"
+          initialPosition={0.08}
+          crawlSpeed={200}
+          height={2}
+          crawl={true}
+          showSpinner={false}
+          easing="ease"
+          speed={100}
+          shadow="0 0 10px #FF9F43,0 0 5px #FF9F43"
+        />
+        <LoadingProvider>
+          <SidebarProvider>
+            <Layout>
+              <ToastContainer
+                position="top-center"
+                autoClose={1000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+              />
+              {children}
+              <section className="my-[20vh]"></section>
+            </Layout>
+          </SidebarProvider>
+        </LoadingProvider>
       </body>
     </html>
   );
