253fe137671b9de76211160b3ae381ea11cc1d2d
Vanshu Galhotra <vanshugalhotra3332@gmail.com>
2024-07-27 10:49:39 +0530

    feat - updatemrp is now working for large datasets

--- db/mongoose.js
+++ db/mongoose.js
@@ -5,8 +5,20 @@ const connectDb = (handler) => async (req, res) => {
     return handler(req, res);
   }
 
-  await mongoose.connect(process.env.MONGO_URI);
-  return handler(req, res);
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 60000, // 60 seconds
+      socketTimeoutMS: 60000, // 60 seconds
+      connectTimeoutMS: 10000, // 10 seconds
+      bufferCommands: false, // Disable buffering commands
+    });
+    return handler(req, res);
+  } catch (error) {
+    console.error("Database connection error:", error);
+    res.status(500).json({ error: "Database connection error" });
+  }
 };
 
 export default connectDb;
