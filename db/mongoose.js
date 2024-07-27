d71f1c624774bcb75fb6f9c89287b41ebedb9442
Vanshu Galhotra <vanshugalhotra3332@gmail.com>
2024-07-27 10:54:21 +0530

    fix - mongoose connection

--- db/mongoose.js
+++ db/mongoose.js
@@ -2,18 +2,18 @@ import mongoose from "mongoose";
 
 const connectDb = (handler) => async (req, res) => {
   if (mongoose.connections[0].readyState) {
+    console.log("Using existing database connection");
     return handler(req, res);
   }
 
   try {
     await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
       serverSelectionTimeoutMS: 60000, // 60 seconds
       socketTimeoutMS: 60000, // 60 seconds
       connectTimeoutMS: 10000, // 10 seconds
       bufferCommands: false, // Disable buffering commands
     });
+    console.log("Established new database connection");
     return handler(req, res);
   } catch (error) {
     console.error("Database connection error:", error);
